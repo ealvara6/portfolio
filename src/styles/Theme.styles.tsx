cc1c050ed8bebaa19ade6f310f9629dfa72015ef
Eduardo Alvarado <ealvara73@gmail.com>
2023-08-17 17:59:36 -0500

    Add additional packages and global css

---
+++ src/styles/Theme.styles.tsx
@@ -0,0 +1,11 @@
+const theme = {
+  dark: {
+    color: '#fff',
+    primaryDark: '#041C32',
+    primaryLight: '#04293A',
+    secondaryDark: '#064663',
+    secondaryLight: '#ECB365',
+  },
+};
+
+export default theme;
