988e61bcb15723b37e8c474c95491e5ff247dc6d
Eduardo Alvarado <ealvara73@gmail.com>
2023-09-26 00:31:42 -0500

    Fix grammar and typos

--- src/components/About/Section.tsx
+++ src/components/About/Section.tsx
@@ -5,23 +5,23 @@ export default function Section() {
   return (
     <StyledSection>
       <Text>
-        Hey there! I&apos;m Eduardo Alvarado. I am an aspiring web developer looking to step
-        my foot into the world of web development. I am an Alum from the University of Houston and
-        I graduated from the C.T Bauer College of Bussiness with a bachelors degree in
-        Management Information Systems in the fall of 2020.
+        Hey there! I&apos;m Eduardo Alvarado. I am an aspiring web developer
+        looking to step foot into the world of web development. I am an alumnus
+        from the University of Houston, and I graduated from the C.T. Bauer College of Business
+        with a bachelor&apos;s degree in Management Information Systems in the fall of 2020.
       </Text>
       <br />
       <Text>
-        I am currently taking courses from the Odin Project (a community driven curriculum)
-        which is teaching me fullstack web development with emphasis on React and Node.
-        I have coded multiple projects to try and solidify my skills in web development and
-        I hope that I can show that I am a hard working individual determined to start my
-        career as a web developer.
+        I am currently taking courses from the Odin Project (a community-driven curriculum),
+        which is teaching me full-stack web development with an emphasis on React and Node.
+        I have coded multiple projects to try and solidify my skills in web development,
+        and I hope that I can show that I am a hard-working individual
+        determined to start my career as a web developer.
       </Text>
       <br />
       <Text>
-        Please feel free to contact me with any questions or opportunities and thank you for taking
-        the time to consider my as a potential developer!
+        Please feel free to contact me with any questions or opportunities,
+        and thank you for taking the time to consider me as a potential developer!
       </Text>
     </StyledSection>
   );
